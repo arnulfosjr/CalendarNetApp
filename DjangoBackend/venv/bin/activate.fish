b8bf4d7b4abe090449d04b21bd826cdda6ec9bcf
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-10-04 19:02:25 -0700

    terminal venv error solved

--- DjangoBackend/venv/bin/activate.fish
+++ DjangoBackend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/arnulfosjr/Desktop/CalendarNetProj/DjangoBackend/venv"
+set -gx VIRTUAL_ENV "/Users/arnulfosjr/CalendarNetApp/DjangoBackend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
