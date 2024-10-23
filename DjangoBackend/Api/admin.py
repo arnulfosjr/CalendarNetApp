033eb3a86e06441325fcf16a5413c01c18f91c54
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-10-23 15:17:11 -0700

    added admin models

--- DjangoBackend/Api/admin.py
+++ DjangoBackend/Api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User,Event,Task,Reminder
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email','password']
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title','startDate','endDate','color','descr']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title','dueDate','color','descr','timeCreated','updateTimeCreated','isCompleted']
+
+@admin.register(Reminder)
+class ReminderAdmin(admin.ModelAdmin):
+    list_display = ['event','task','time','timeCreated','updateTimeCreated']
+
