fee105c2c9b7f039ba121e96e83e3f1bb97ef255
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-10-23 15:02:16 -0700

    Database models added

--- DjangoBackend/Api/models.py
+++ DjangoBackend/Api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=12)
+
+class Event(models.Model):
+    title = models.CharField(max_length=20)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    color = models.CharField()
+    descr = models.CharField(max_length=200)
+
+class Task(models.Model):
+    title = models.CharField(max_length=20)
+    dueDate = models.DateField()
+    color = models.CharField()
+    descr = models.CharField(max_length=200)
+    timeCreated = models.DateTimeField(auto_now_add=True)
+    updateTimeCreated = models.DateTimeField(auto_now=True)
+    isCompleted = models.BooleanField(default=False)
+
+class Reminder(models.Model):
+    event = models.ForeignKey('Event',on_delete=models.CASCADE,related_name='reminders',null=True,blank=True)
+    task = models.ForeignKey('Task',on_delete=models.CASCADE,related_name='reminders',null=True,blank=True)
+    time = models.DateTimeField()
+    timeCreated = models.DateTimeField(auto_now_add=True)
+    updateTimeCreated = models.DateTimeField(auto_now=True)
+
+    
