c97598f5cfca3d0e37eab35812ca3df69519bb6f
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-10-22 20:42:05 -0700

    removed duplicate expo folder

--- CalendarNet/app/Calendar.js
+++ CalendarNet/app/Calendar.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import {View,Text} from 'react-native';
 import styles from './src/styles/styles';
+import {format} from 'date-fns';
 
-const element = () => {
+
+const Months = [
+    {month:'January',monthAcro:'Jan', days:31},
+    {month:'February',monthAcro:'Feb', days:28},
+    {month:'March',monthAcro:'Mar', days:31},
+    {month:'April',monthAcro:'Apr', days:30},
+    {month:'May',monthAcro:'May', days:30},
+    {month:'June',monthAcro:'Jun', days:31},
+    {month:'July',monthAcro:'Jul', days:31},
+    {month:'August',monthAcro:'Aug', days:31},
+    {month:'September',monthAcro:'Sep', days:30},
+    {month:'October',monthAcro:'Oct', days:31},
+    {month:'November',monthAcro:'Nov', days:30},
+    {month:'December',monthAcro:'Dec', days:31}
+];
+
+const LeapYear = () => {
+
+}
+const calendarUI = () => {
     return(
         <View style={styles.container}>
             <Text style={styles.text}>Hello World!</Text>
@@ -11,4 +31,4 @@ const element = () => {
     );
 };
 
-export default element;
+export default calendarUI;
