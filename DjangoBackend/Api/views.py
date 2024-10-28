d1809b3956575ead9bff4d950a948f8047173340
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-10-28 13:34:21 -0700

    added view classes, restframework settings

--- DjangoBackend/Api/views.py
+++ DjangoBackend/Api/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import response
 
-# Create your views here.
+class UserCreate():
+    pass
+
+class UserEdit():
+    pass
+
+class UserAccountView(APIView):
+    pass
+
+class UserDelete():
+    pass
+
+class EventCreate():
+    pass
+
+class EventEdit():
+    pass
+
+class EventDelete():
+    pass
+
+class EventView(APIView):
+    pass
+
+class TaskCreate():
+    pass
+
+class TaskEdit():
+    pass
+
+class TaskDelete():
+    pass
+
+class TaskView(APIView):
+    pass
+
+class ReminderCreate():
+    pass
+
+class ReminderEdit():
+    pass
