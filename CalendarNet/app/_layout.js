f90d1595f3494990a4662bd8aadeb2a1ce32aeeb
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-10-30 22:34:39 -0700

    layout.js changes

--- CalendarNet/app/_layout.js
+++ CalendarNet/app/_layout.js
@@ -3,8 +3,8 @@ import { Stack } from 'expo-router';
 export default function Layout(){
     return (
         <Stack>
-            <Stack.Screen name='UserAccess' />
-            <Stack.Screen name='Calendar' />
+            <Stack.Screen name='UserAccess' options={{ headerShown:false}} />
+            <Stack.Screen name='Calendar' options={{title: 'Calendar'}}/>
         </Stack>
     );
 }
