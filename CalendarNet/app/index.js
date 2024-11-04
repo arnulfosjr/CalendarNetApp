7ce5e00ded522d21c8f2d40335d6e6c2acc08863
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-11-03 21:03:22 -0800

    index.js is back

---
+++ CalendarNet/app/index.js
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router';
+import UserAccess from './screens/UserAccess';
+import Calendar from './screens/Calendar';
+
+export default function Index() {
+    return <Calendar />;
+    
+}
