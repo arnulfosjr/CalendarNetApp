90577f4d4e67bb8b8b7fbe7f2b17861acc463d93
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-11-03 16:24:30 -0800

    removed index.js to remove app header

--- CalendarNet/app/_layout.js
+++ CalendarNet/app/_layout.js
@@ -3,8 +3,8 @@ import { Stack } from 'expo-router';
 export default function Layout(){
     return (
         <Stack>
-            <Stack.Screen name='screens/UserAccess' options={{ headerShown:false}} />
-            <Stack.Screen name='screens/Calendar' options={{ headerShown:false }}/>
+            <Stack.Screen name='screens/UserAccess' options={{headerShown: false}}/>
+            <Stack.Screen name='screens/Calendar' options={{headerShown: false}}/>
         </Stack>
     );
 }
--- CalendarNet/app/index.js
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-import UserAccess from './screens/UserAccess'
-import Calendar from './screens/Calendar'
-
-
-export default function Index() {
-    return <UserAccess/>;
-
-}
