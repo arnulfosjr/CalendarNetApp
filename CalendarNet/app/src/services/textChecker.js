49d95633f42e8eaaa25a969cd2c41ac785a2858d
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-11-06 21:24:02 -0800

    textChecker.js fix

--- CalendarNet/app/src/services/textChecker.js
+++ CalendarNet/app/src/services/textChecker.js
@@ -4,5 +4,5 @@ const sentence = "Create dentist appointment on November 9, 2024 at 10:00 AM.";
 
 const doc = nlp(sentence);
 
-const date = doc.dates().out('array');
+const date = doc.match('#Date').out('array');
 console.log(date);
